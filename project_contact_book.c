774ca06d80cd7456918e1505b8acdd71054c956c
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2022-09-19 22:21:14 -0300

    Sort contacts

--- project_contact_book.c
+++ project_contact_book.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX_CONTACTS 3
+#define MAX_CONTACTS 100
 
 struct contact
 {
@@ -16,13 +16,19 @@ struct contact contacts[MAX_CONTACTS];
 int total_contacts = 0;
 
 // Insere um contato
-void insert()
+int insert()
 {
+    if (total_contacts == MAX_CONTACTS)
+    {
+        printf("Cannot insert new records\n"); // Lista de contatos preenchida
+        return 0;
+    }
     printf("Contact name: ");
     scanf("%s", contacts[total_contacts].name);
     contacts[total_contacts].id = total_contacts + 1;
     total_contacts++;
     printf("\n");
+    return 1; // Contato adicionado
 }
 
 // Mostra todos os contatos
@@ -39,33 +45,50 @@ void show()
     printf("\n");
 }
 
-// Busca um contato por id
-int search(int id)
+// Mostra os contatos por ordem alfabética
+void sort()
+{
+    int i, j, r;
+    char aux[100];
+    for (i = 0; i < MAX_CONTACTS - 1; i++)
+    {
+        for (j = 0; j < MAX_CONTACTS - i - 1; j++)
+        {
+            r = strcmp(contacts[j].name, contacts[j + 1].name);
+            if (r > 0)
+            {
+                strcpy(aux, contacts[j].name);
+                strcpy(contacts[j].name, contacts[j + 1].name);
+                strcpy(contacts[j + 1].name, aux);
+            }
+        }
+    }
+    show();
+}
+
+// Procura um contato por nome
+int search(char name[100])
 {
     int i;
     for (i = 0; i < MAX_CONTACTS; i++)
     {
-        if (id == contacts[i].id)
+        if (strcmp(name, contacts[i].name) == 0)
         {
-            return id; // Retorna o id do contato buscado
+            return i + 1;
         }
     }
-    return 0; // Contato não encontrado
+    return 0;
 }
 
 // Deleta um contato por id
 int delete (int id)
 {
-    if (search(id) == 0)
-    {
-        return 0; // Contato não encontrado
-    }
     int i;
     for (i = 0; i < MAX_CONTACTS; i++)
     {
         if (id == contacts[i].id)
         {
-            contacts[i].id = NULL;
+            contacts[i].id = 0;
             return 1; // Contato deletado
         }
     }
@@ -77,11 +100,12 @@ int menu()
 {
     int response = 0;
     printf("--- MENU ---\n");
-    printf("1 - Insert contact\n");
-    printf("2 - Delete contact\n");
-    printf("3 - Search contact\n");
-    printf("4 - Show contact(s)\n");
-    printf("5 - Exit\n");
+    printf("1 - List people\n");
+    printf("2 - List People in Alphabetical Order\n");
+    printf("3 - Search Person by name\n");
+    printf("4 - Insert Person\n");
+    printf("5 - Remove Person by id\n");
+    printf("6 - Exit\n");
     printf("Enter a option: ");
     scanf("%d", &response);
     printf("\n");
@@ -90,17 +114,39 @@ int menu()
 
 int main(void)
 {
-    int i, id_contact_delete, id_contact_wanted;
+    int id_contact_delete;
+    char name_contact_wanted[100];
 
     int option = menu();
-    while (option != 5)
+    while (option != 6)
     {
         switch (option)
         {
         case 1:
-            insert();
+            show();
             break;
         case 2:
+            sort();
+            break;
+        case 3:
+            printf("Search a name: ");
+            scanf("%s", name_contact_wanted);
+            int wanted = search(name_contact_wanted);
+            if (wanted == 0)
+            {
+                printf("\nContact not found!\n\n");
+            }
+            else
+            {
+                printf("\nContact found!\n");
+                printf("Id: %d, Name: %s\n\n", contacts[wanted - 1].id, contacts[wanted - 1].name);
+            }
+            break;
+        case 4:
+            insert();
+
+            break;
+        case 5:
             printf("Delete an id: ");
             scanf("%d", &id_contact_delete);
 
@@ -113,24 +159,6 @@ int main(void)
             {
                 printf("\nContact deleted\n\n");
             }
-            break;
-        case 3:
-            printf("Search an id: ");
-            scanf("%d", &id_contact_wanted);
-
-            int wanted = search(id_contact_wanted);
-            if (wanted == 0)
-            {
-                printf("\nContact not found!\n\n");
-            }
-            else
-            {
-                printf("\nContact found!\n");
-                printf("Id: %d, Name: %s\n\n", contacts[wanted - 1].id, contacts[wanted - 1].name);
-            }
-            break;
-        case 4:
-            show(contacts);
             break;
         default:
             break;
